4bed632814a349ae01004d69b65a6337da70d2fd
abhinavkumar03 <ak2711474@gmail.com>
2026-02-15 20:35:43 +0530

    updated skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -27,7 +27,7 @@ export default function Skills() {
                     </p>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                     {skills.map((group, index) => {
                         const CategoryIcon = getIcon(group.icon);
 
